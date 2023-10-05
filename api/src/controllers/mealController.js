d548aa347fdff92ff5ed88e4e973781f1c98d8d3
Lucas G Accurcio Ferreira <lucas.ferreira@brisabr.com.br>
2023-10-05 09:58:46 -0300

    refactor: return api messages

--- api/src/controllers/mealController.js
+++ api/src/controllers/mealController.js
@@ -2,36 +2,36 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 exports.createMeal = async (req, res) => {
-  const { userId, name, calories, time } = req.body;
-  try {
-    const newMeal = await prisma.meal.create({
-      data: { userId, name, calories, time }
-    });
-    res.json(newMeal);
-  } catch (error) {
-    res.status(500).json({ message: 'Could not create meal', error });
-  }
+	const { userId, name, calories, time } = req.body;
+	try {
+		const newMeal = await prisma.meal.create({
+			data: { userId, name, calories, time },
+		});
+		res.json(newMeal);
+	} catch (error) {
+		res.json({ status: 500, message: 'Could not create meal', error });
+	}
 };
 
 exports.updateMeal = async (req, res) => {
-  const { id, name, calories, time } = req.body;
-  try {
-    const updatedMeal = await prisma.meal.update({
-      where: { id },
-      data: { name, calories, time }
-    });
-    res.json(updatedMeal);
-  } catch (error) {
-    res.status(500).json({ message: 'Could not update meal', error });
-  }
+	const { id, name, calories, time } = req.body;
+	try {
+		const updatedMeal = await prisma.meal.update({
+			where: { id },
+			data: { name, calories, time },
+		});
+		res.json(updatedMeal);
+	} catch (error) {
+		res.json({ status: 500, message: 'Could not update meal', error });
+	}
 };
 
 exports.deleteMeal = async (req, res) => {
-  const { id } = req.params;
-  try {
-    await prisma.meal.delete({ where: { id: parseInt(id) } });
-    res.json({ message: 'Meal deleted' });
-  } catch (error) {
-    res.status(500).json({ message: 'Could not delete meal', error });
-  }
+	const { id } = req.params;
+	try {
+		await prisma.meal.delete({ where: { id: parseInt(id) } });
+		res.json({ status: 200, message: 'Meal deleted' });
+	} catch (error) {
+		res.json({ status: 500, message: 'Could not delete meal', error });
+	}
 };
